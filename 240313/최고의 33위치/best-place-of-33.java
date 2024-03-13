071e2dfa4abc33d0856702dfc24e715b4b885418
시은 <sieunnnn1120@naver.com>
2024-03-13 23:13:31 +0900

    Title: 최고의 33위치, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240313/최고의 33위치/best-place-of-33.java
+++ 240313/최고의 33위치/best-place-of-33.java
@@ -7,6 +7,9 @@ public class Main {
     static int[][] graph;
     static int N;
     static int max;
+    static int coinNum;
+    static int x;
+    static int y;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
@@ -16,13 +19,31 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
-                
+            }
+        }
+
+        solve();
+        System.out.println(max);
+    }
+
+    public static void solve() {
+        for (int i = 0; i < N; i ++) {
+            for (int j = 0; j < N; j ++) {
+                coinNum = 0;
+                checkCoin(i, j);
+            }
+        }
+    }
+
+    public static void checkCoin(int x, int y) {
+        for (int i = y; i < N; i ++) {
+            for (int j = x; j < N; j ++) {
                 if (graph[i][j] == 1) {
-                    max++;
+                    coinNum ++;
                 }
             }
         }
 
-        System.out.println(max);
+        max = Math.max(max, coinNum);
     }
 }
