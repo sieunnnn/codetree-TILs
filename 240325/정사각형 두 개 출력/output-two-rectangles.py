368f4023f88a1e2d99b726ec1e44d03b9d72a740
시은 <sieunnnn1120@naver.com>
2024-03-25 18:43:13 +0900

    Title: 정사각형 두 개 출력, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240325/정사각형 두 개 출력/output-two-rectangles.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for _ in range(n) :
+    for i in range(n) :
+        print("*", end="")
+    print("")
+
+print("")
+
+for _ in range(n) :
+    for i in range(n) :
+        print("*", end="")
+    print("")
