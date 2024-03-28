a40534598ae225331d2ad25ad0db2649cc44086c
시은 <sieunnnn1120@naver.com>
2024-03-28 20:41:48 +0900

    Title: 양수 직사각형의 최대 크기, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240328/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static int m;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int max = Integer.MIN_VALUE;
+    static int cnt = 0;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        graph = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        makeBox();
+        
+        System.out.println(max);
+    }
+
+    public static void makeBox() {
+        for (int i = 0; i < n; i++) { // y1
+            for (int j = 0; j < m; j++) { // x1
+                for (int k = i; k < n; k++) { // y2
+                    for (int s = j; s < m; s++) { // x2
+                        if(checkValues(j, i, s, k)) {
+                            max = Math.max(max, cnt);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean checkValues(int x1, int y1, int x2, int y2) {
+        cnt = 0;
+        for (int i = y1; i <= y2; i++) {
+            for(int j = x1; j <= x2; j++) {
+                if(graph[i][j] < 0) {
+                    return false;
+                } else {
+                    cnt ++;
+                }
+            }
+        }
+        return true;
+    }
+}
