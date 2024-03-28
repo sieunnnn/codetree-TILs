847b009b45536e571de858858f375219eb341bab
시은 <sieunnnn1120@naver.com>
2024-03-28 16:02:10 +0900

    Title: 알파벳 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240328/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,14 @@
+from string import ascii_uppercase
+
+n = int(input()) 
+alpha_list = list(ascii_uppercase)
+current_index = 0
+
+for j in range(n):
+    for k in range(n):
+        if k < j:
+            print(" ", end=" ")
+        else:
+            print(alpha_list[current_index], end=" ")
+            current_index += 1
+    print()
