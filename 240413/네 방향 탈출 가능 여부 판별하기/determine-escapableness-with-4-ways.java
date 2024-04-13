ac8fcec480c98a250cbdaa7183425d1a2eedf4e6
시은 <sieunnnn1120@naver.com>
2024-04-13 11:34:17 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 178ms, Memory: 10MB, Status: Passed - CodeTree

--- 240413/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 240413/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -25,28 +25,33 @@ public class Main {
             }
         }
 
-        int answer = solve(0, 0);
-        System.out.println(answer);
+        if (graph[0][0] == 1) {
+            int answer = solve(0, 0);
+            System.out.println(answer);
+        } else {
+            System.out.println(0); 
+        }
     }
 
     public static int solve(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{x, y});
         visited[y][x] = true;
-        
+
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
 
             if (current[0] == m - 1 && current[1] == n - 1) {
                 return 1;
             }
-            
-            for(int i = 0; i < 4; i++) {
+
+            for (int i = 0; i < 4; i++) {
                 int nx = current[0] + dx[i];
                 int ny = current[1] + dy[i];
 
-                if (nx >= 0 && nx < m && ny >=0 && ny < n) {
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
                     if (!visited[ny][nx] && graph[ny][nx] == 1) {
+                        visited[ny][nx] = true;
                         queue.add(new int[]{nx, ny});
                     }
                 }
