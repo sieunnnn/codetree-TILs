499bc0cb1a837da319d8439a0f6d7d1876fb40db
시은 <sieunnnn1120@naver.com>
2024-04-30 16:16:52 +0900

    Title: 양수 직사각형의 최대 크기, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

--- 240430/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 240430/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -1,64 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
     static int n, m;
     static int[][] graph;
     static int max_size = -1;
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
+
         graph = new int[n][m];
         initGraph();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (graph[i][j] < 0) {
-                    break;
-                }
-                
-                for (int k = i; k < n; k++) {
-                    for (int s = j; s < m; s++) {
-                        if (graph[k][s] < 0) {
-                            break;
-                        }
-                        findMaxSize(i, j, k, s);
-                    }
+                if (graph[i][j] > 0) {
+                    findMaxSize(i, j);
                 }
             }
         }
 
-        System.out.println(max_size);
+        System.out.println(max_size == -1 ? -1 : max_size);
     }
 
     public static void initGraph() throws IOException {
-        for (int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
             for (int j = 0; j < m; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
     }
 
-    public static void findMaxSize(int y1, int x1, int y2, int x2) {
-        boolean checkSize = true;
-        int size = 0;
+    public static void findMaxSize(int startY, int startX) {
+        int minWidth = Integer.MAX_VALUE;
 
-        for (int i = y1; i <= y2; i++) {
-            for (int j = x1; j <= x2; j++) {
-                if (graph[i][j] < 0) {
-                    checkSize = false;
-                }
+        for (int i = startY; i < n && graph[i][startX] > 0; i++) {
+            int width = 0;
+
+            while (startX + width < m && graph[i][startX + width] > 0) {
+                width++;
             }
-        }
-        
-        if (checkSize) {
-            size = (x2 - x1 + 1) * (y2 - y1 + 1);
+
+            minWidth = Math.min(minWidth, width);
+            int size = minWidth * (i - startY + 1);
             max_size = Math.max(max_size, size);
         }
     }
