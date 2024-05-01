8a09a7141c6548bed8ee8018c5f0c8149a65a3d1
시은 <sieunnnn1120@naver.com>
2024-05-01 15:32:31 +0900

    Title: 핀볼게임, Time: 115ms, Memory: 9MB, Status: Passed - CodeTree

--- 240501/핀볼게임/pinball-game.java
+++ 240501/핀볼게임/pinball-game.java
@@ -2,63 +2,93 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[][] arr = new int[101][101];
-    static int N;
-    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static final int UP = 0;
-    static final int DOWN = 1;
-    static final int LEFT = 2;
-    static final int RIGHT = 3;
-    static int max = 0;
-    static int[][] directionMap = new int[][] {
-            {UP, DOWN, LEFT, RIGHT},
-            {RIGHT, LEFT, DOWN, UP},
-            {LEFT, RIGHT, UP, DOWN}
-    };
+
+    static Map<String, String> map1 = new HashMap<>();
+    static Map<String, String> map2 = new HashMap<>();
+
+    static int N;
+    static int maxTime = Integer.MIN_VALUE;
+    static int[][] graph;
+
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(bf.readLine());
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        initMaps();
+
         for (int i = 0; i < N; i++) {
-            // DOWN
-            solve(DOWN, 0, i, 1);
-            // UP
-            solve(UP, N-1, i, 1);
-            // LEFT
-            solve(LEFT, i, N-1, 1);
-            // RIGHT
-            solve(RIGHT, i, 0, 1);
+            findMaxTime(0, i, "right", 1, true);
+            findMaxTime(i, 0, "down", 1, true);
+            findMaxTime(N - 1, i, "left", 1, true);
+            findMaxTime(i, N - 1, "up", 1, true);
         }
-        System.out.println(max);
+
+        System.out.println(maxTime);
     }
-    public static void solve(int direction, int i, int j, int acc) {
-        if (i < 0 || i >= N || j < 0 || j >= N) {
-            max = Math.max(max, acc);
-            return;
+
+    public static void initMaps() {
+        map1.put("up", "right");
+        map1.put("down", "left");
+        map1.put("left", "down");
+        map1.put("right", "up");
+
+        map2.put("up", "left");
+        map2.put("down", "right");
+        map2.put("left", "up");
+        map2.put("right", "down");
+    }
+
+    public static void findMaxTime(int x, int y, String direction, int time, boolean start) {
+        if (!start)  {
+            if (x < 0 || x >= N || y < 0 || y >= N) {
+                maxTime = Math.max(maxTime, time);
+                return;
+            }
         }
-        int nextDirection = findNextDirection(direction, arr[i][j]);
-        int[] nextPosition = findNextPosition(nextDirection, i, j);
-        solve(nextDirection, nextPosition[0], nextPosition[1], acc + 1);
-    }
-    public static int findNextDirection (int direction, int kind) {
-        return directionMap[kind][direction];
-    }
-    public static int[] findNextPosition (int direction, int i, int j) {
-        int[] ret = new int[] {i, j};
-        if (direction == UP) {
-            ret[0]--;
-        } else if (direction == DOWN) {
-            ret[0]++;
-        } else if (direction == LEFT) {
-            ret[1]--;
-        } else if (direction == RIGHT) {
-            ret[1]++;
+
+        int currentValue = graph[y][x];
+        String nextDirection = findNextDirection(currentValue, direction);
+
+        if (nextDirection.equals("up")) {
+            findMaxTime(x, y - 1, nextDirection, time + 1, false);
+
+        } else if (nextDirection.equals("down")) {
+            findMaxTime(x, y + 1, nextDirection, time + 1, false);
+
+        } else if (nextDirection.equals("left")) {
+            findMaxTime(x - 1, y, nextDirection, time + 1, false);
+
+        } else if (nextDirection.equals("right")) {
+            findMaxTime(x + 1, y, nextDirection, time + 1, false);
+        } else {
+            System.out.println("!!");
         }
-        return ret;
+    }
+
+    public static String findNextDirection(int currentValue, String direction) {
+        String nextDirection = "";
+
+        if (currentValue == 0) {
+            nextDirection = direction;
+
+        } else if (currentValue == 1) {
+            nextDirection = map1.get(direction);
+
+        } else if (currentValue == 2) {
+            nextDirection = map2.get(direction);
+        } else {
+            System.out.println("!!!!!");
+        }
+
+        return nextDirection;
     }
 }
