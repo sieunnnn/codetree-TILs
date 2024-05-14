717ed87bc6289f6c064a285d4f42937de9890a6e
시은 <sieunnnn1120@naver.com>
2024-05-14 14:50:25 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240514/계단 오르기/climbing-stairs.java
+++ 240514/계단 오르기/climbing-stairs.java
@@ -9,8 +9,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        makeFibonacci();
-        System.out.println(fibonacci[N]);
+        if (N == 1) {
+            System.out.println(0);
+
+        } else {
+            makeFibonacci();
+            System.out.println(fibonacci[N]);
+        }
     }
 
     public static void makeFibonacci() {
